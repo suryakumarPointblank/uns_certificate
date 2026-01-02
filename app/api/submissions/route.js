e6c0849eaa4af420bc1727ad287eddb6e9ae0c3b
suryakumarPointblank <surya@pointblank.co.in>
2026-01-02 11:58:35 +0530

    fixed bug in count updation

--- app/api/submissions/route.js
+++ app/api/submissions/route.js
@@ -26,7 +26,7 @@ export async function POST(request) {
 
     return NextResponse.json({
       success: true,
-      count: count+214
+      count: count+213
     });
   } catch (error) {
     console.error('Error saving submission:', error);
